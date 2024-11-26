0b8e3c054887d32a02e9699570d6eaa5aa079efc
Hlib Hryshko <glebg27@gmail.com>
2024-11-26 16:06:45 +0100

    Initial commit

---
+++ src/test/java/com/example/java_ecommerce/JavaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
